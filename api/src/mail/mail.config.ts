009c152a8e7c535515cc58bef94a6343b1f55102
nami-kaze <namitagarg2003@gmail.com>
2025-02-06 00:49:09 +0530

    verification email

--- api/src/mail/mail.config.ts
+++ api/src/mail/mail.config.ts
@@ -1,9 +1,14 @@
 export const mailTransportConfig = {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT ? parseInt(process.env.MAIL_PORT, 10) : 465,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 5000
 }
